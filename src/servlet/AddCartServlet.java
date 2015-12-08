867b5b62799c73b3c9782e33c0cbe791d4b8f897
horrorental <jojo.agogo@hotmail.co.jp>
2015-12-08 17:49:25 +0900

    kurosaki

--- src/servlet/AddCartServlet.java
+++ src/servlet/AddCartServlet.java
@@ -37,6 +37,8 @@ public class AddCartServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//kurosaki
+		
 		String msg = null;
 		String flg = null;
 		request.setCharacterEncoding("UTF-8");
